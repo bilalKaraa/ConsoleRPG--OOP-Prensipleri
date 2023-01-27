c221e12df2d518e3f827482e69e953c783275a44
bilalKaraa <bilal.2164@outlook.com>
2023-01-27 15:22:39 +0300

    oyun konsol ekranı özelleştirildi. kullanıcı adı ve şifreyle giriş eklendi. karakter seçimi eklendi.

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -12,13 +12,65 @@ namespace ConsoleRPG
     {
         static void Main(string[] args)
         {
-            Irk ı = new Irk("insan");
-            Sinif s = new Sinif("savasci");
-            Karakter k = new Karakter(s, ı);
-            Console.WriteLine(k.YakinSaldiri());
-            Console.Read();
-                
-       
+            Sinif savasci = new Sinif("savasci");
+            Sinif okcu = new Sinif("okcu");
+            Sinif buyucu = new Sinif("buyucu");
+            Sinif sovalye = new Sinif("sovalye");
+            Sinif ninja = new Sinif("ninja");
+
+            Irk insan = new Irk("insan");
+            Irk elf = new Irk("elf");
+            Irk ork = new Irk("cuce");
+            Irk undead = new Irk("undead");
+           
+            bool dogru=false;
+            while (!dogru)
+            {
+                Console.WriteLine("Lütfen giriş yapmak için kulanıcı isminizi giriniz!");
+                string isim = Console.ReadLine();
+                Console.WriteLine("Lütfen şifenizi giriniz!");
+                string sifre = Console.ReadLine();
+
+                if (isim == "Bilal" && sifre == "1234")
+                {
+                    dogru= true;    
+                    Console.WriteLine("Oyuna hoşgeldin... Karakter yaratmaya hazırsın. Karakterinin ismini giriniz!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı Bulunamadı Bilgileri Kontrol Ediniz!");
+                    dogru= false;
+                }
+            }
+
+            string karakterİsmi = Console.ReadLine();
+            string sinifSecimi;
+            Sinif secilenSinif = null;
+            do
+            {
+                Console.WriteLine("Karakterinizin sınıfını seçiniz \n 1: Savasci \n 2:Okcu \n 3: Buyucu \n 4:Sovalye \n 5:Ninja ... FARKLI BİR SEÇENEK BULUNMADIĞINDAN VAR OLAN SINIFLAR İLE İLERLEYEBİLİRİZ !");
+                sinifSecimi = Console.ReadLine();
+                switch (sinifSecimi)
+                {
+                    case "1":
+                        secilenSinif = savasci;
+                        break;
+                    case "2":
+                        secilenSinif = okcu;
+                        break;
+                    case "3":
+                        secilenSinif = buyucu;
+                        break;
+                    case "4":
+                        secilenSinif = sovalye;
+                        break;
+                    case "5":
+                        secilenSinif = ninja;
+                        break;
+                }
+
+            } while (sinifSecimi != "1" && sinifSecimi != "2" && sinifSecimi != "3" && sinifSecimi != "4" && sinifSecimi != "5");
         }
     }
 }
